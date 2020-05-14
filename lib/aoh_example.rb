95635754fec587cf25a264ebf6b079986c3ee87d
Travis Prol <travisjohnprol@gmail.com>
2020-05-14 20:37:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -34,7 +34,7 @@ end
 require 'pry'
 def aoh_update(aoh, row, key, new_value)
   aoh[row][:key] = new_value
-  binding.binding.pry
+  binding.pry
   aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
