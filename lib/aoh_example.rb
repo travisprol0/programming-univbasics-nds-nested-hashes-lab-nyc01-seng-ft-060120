f5a242b1a569afe5bbe979b22fd70692c32441f2
Travis Prol <travisjohnprol@gmail.com>
2020-05-14 20:30:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,13 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
+  array = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]# Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
